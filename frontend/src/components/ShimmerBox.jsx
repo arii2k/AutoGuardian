495cdddfd3ef199344ab9b8460f8d1e2c26144da
arii2k <130521458+arii2k@users.noreply.github.com>
2025-11-29 21:12:56 +0100

    Delete frontend/src directory

--- frontend/src/components/ShimmerBox.jsx
+++
@@ -1,7 +0,0 @@
-export default function ShimmerBox({ height = "h-6", width = "w-full", className = "" }) {
-  return (
-    <div
-      className={`shimmer rounded-lg ${height} ${width} bg-white/5 ${className}`}
-    />
-  );
-}
